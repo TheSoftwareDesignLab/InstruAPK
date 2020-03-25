f240fe4528663787895a9836f00ce590728b8389
Camilo <ca.escobar2434@uniandes.edu.co>
2020-03-25 12:59:33 -0500

    remove empty methods from scope

--- src/uniandes/tsdl/instruapk/helper/ASTHelper.java
+++ src/uniandes/tsdl/instruapk/helper/ASTHelper.java
@@ -111,8 +111,14 @@ public class ASTHelper {
 		if(t.getType()==smaliParser.I_METHOD) {
 //			System.out.println(t.getType()+" "+t.toStringTree());
 //			CommonTree tt = (CommonTree) t.getFirstChildWithType(smaliParser.I_ORDERED_METHOD_ITEMS);
-//			System.out.println(tt.getLine());
-			resp.add(39);
+//			System.out.println(t.getLine());
+//			System.out.println(t.getFirstChildWithType(smaliParser.I_ORDERED_METHOD_ITEMS).getChildCount());
+//			System.out.println(t.toStringTree());
+			if(t.getFirstChildWithType(smaliParser.I_ORDERED_METHOD_ITEMS).getChildCount()>0) {
+				System.out.println(t.getChild(0).toStringTree());
+				System.out.println(t.getLine()+" "+t.getFirstChildWithType(smaliParser.I_ORDERED_METHOD_ITEMS).getLine());
+				resp.add(39);				
+			}
 		}
 		//		if(t.getType()==smaliParser.I_STATEMENT_FORMAT35c_METHOD) {
 		//			if(t.getFirstChildWithType(smaliParser.I_REGISTER_LIST).getChildCount()==3 
