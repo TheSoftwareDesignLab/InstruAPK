4f19c55ce0ff40973f480a897eebd269c91f7592
Camilo <ca.escobar2434@uniandes.edu.co>
2020-03-25 11:54:44 -0500

    second version of instrumentation

--- src/uniandes/tsdl/instruapk/operators/code/Instrumenter.java
+++ src/uniandes/tsdl/instruapk/operators/code/Instrumenter.java
@@ -1,6 +1,7 @@
 package uniandes.tsdl.instruapk.operators.code;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,42 +20,50 @@ public class Instrumenter implements MutationOperator {
 
 	@Override
 	public boolean performMutation(MutationLocation location, BufferedWriter writer, int mutantIndex) throws Exception {
-		
-		
+
+
 		ASTMutationLocation mLocation = (ASTMutationLocation) location;
 		CommonTree tt = (CommonTree) mLocation.getTree().getFirstChildWithType(smaliParser.I_ORDERED_METHOD_ITEMS);
-		
-//		ASTMutationLocation mLocation = (ASTMutationLocation) location;
-//		CommonTree tree = mLocation.getTree();
-//		CommonTree treee = ASTHelper.getFirstBrotherNamedOfType(smaliParser.I_STATEMENT_FORMAT11x, "move-result-object", tree);
-//		String varName = treee.getChild(1).toString();
-		
+		CommonTree t = mLocation.getTree();
+
 		List<String> newLines = new ArrayList<String>();
 		List<String> lines = FileHelper.readLines(location.getFilePath());
 
 		//Add lines before the MutationLocation
-		for(int i=0; i < tt.getLine()-1; i++){
+		for(int i=0; i < t.getLine()-1; i++){
 			newLines.add(lines.get(i));
 		}
-
-		System.out.println(((ASTMutationLocation) location).getTree().getClass());
-		newLines.add("		sget-object v0, Ljava/lang/System;->out:Ljava/io/PrintStream;\n" +
-				"\n" +
-				"    const-string v1, \"RIP:" + mutantIndex + ":" + "" + ":" + ""+ ":" + new Date(System.currentTimeMillis()) + "\"\n" +
-				"\n" +
-				"    invoke-virtual {v0, v1}, Ljava/io/PrintStream;->println(Ljava/lang/String;)V \n");
+		int iter = t.getLine()-1;
+		String cLine = lines.get(iter);
+		while(!cLine.startsWith(".method") && !cLine.contains(t.getChild(0).getText())) {
+			newLines.add(lines.get(iter));
+			iter++;
+			cLine = lines.get(iter);
+		}
+		for (int i = iter; i < (iter+(tt.getLine()-t.getLine())); i++) {
+			newLines.add(lines.get(i));
+		}
+		iter=(iter+(tt.getLine()-t.getLine()));
 		
-		for(int i=tt.getLine(); i < lines.size() ; i++){
+		newLines.add("		sget-object v0, Ljava/lang/System;->out:Ljava/io/PrintStream;");
+		newLines.add("");
+		newLines.add("    const-string v1, \"RIP:" + mutantIndex + ":" + (new File(mLocation.getFilePath())).getName() + ":" + t.getChild(0).toStringTree()+"\"");
+		newLines.add("");
+		newLines.add("    invoke-virtual {v0, v1}, Ljava/io/PrintStream;->println(Ljava/lang/String;)V");
+		newLines.add("");
+
+
+		for(int i=iter; i < lines.size() ; i++){
 			newLines.add(lines.get(i));
 		}
-//
+		//
 		FileHelper.writeLines(location.getFilePath(), newLines);
 		Helper.mutationSuccess(mutantIndex);
 		Helper.writeBasicLogInfo(mutantIndex, location.getFilePath(), location.getType().getName(), new int[] {mLocation.getLine(),tt.getLine()}, writer);
 		writer.write("	For mutant "+mutantIndex+" Bluetooth Adapter has been set to null");
 		writer.newLine();
 		writer.flush();
-		
+
 		return true;
 	}
 
